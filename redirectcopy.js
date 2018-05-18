fd7b01ab02af1a4e83db7bd5abcfab783bc14524
Phamanhtuan1904 <33902478+Phamanhtuan1904@users.noreply.github.com>
2018-05-18 09:04:22 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Muốn Copy thì vào link này : <a href='http://noithatphuongthao.net/'>http://noithatphuongthao.net/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Muốn Copy thì vào link này : <a href='https://muabanxetai.vn/xe-tai'>https://muabanxetai.vn/xe-tai"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
